9e32d669b666f812dd51a7609e5eab2598063f80
UrbanProfessional <spytron2005@gmail.com>
2022-11-21 20:15:38 -0500

    Confirm

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-var bool = false;
 
+// Function to switch to the phishing test, 'fake' being the div that holds it, and 'main' being the page that first loaded.
 function switchP() {
     document.getElementById("fake").style.display = "block";
     document.getElementById("main").style.display = "none";
 }
 
+// Reverse of first function
 function switchR() {
     document.getElementById("fake").style.display = "none";
     document.getElementById("main").style.display = "block";
 }
 
-
+// Extra functionality for the DOE phishing test
 function doealert()
 {
     document.getElementById("doealert").style.display = "block";
